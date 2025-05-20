ff04c5ea22fc61e7a01c098a9def354eb8589782
Banele Mkhize <mkhizeb174@gmail.com>
2025-05-20 07:04:16 +0200

    done with vector add

--- PractTest1Prac/vector_add.c
+++ PractTest1Prac/vector_add.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <omp.h>
 #include <stdlib.h>
 
-void vec_add(float *a, float *b, float *c, int n)
+void vec_add(float *a, float *b, float *c, long long n)
+{
+// Loop over elements: compute c[i] = a[i] + b[i]
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i)
+    {
+        c[i] = a[i] + b[i]; // ▶️ Hint: each iteration independent
+    }
+}
+void vec_add_serial(float *a, float *b, float *c, long long n)
 {
     // Loop over elements: compute c[i] = a[i] + b[i]
+    // #pragma omp parallel for
     for (int i = 0; i < n; ++i)
     {
         c[i] = a[i] + b[i]; // ▶️ Hint: each iteration independent
@@ -12,7 +23,7 @@ void vec_add(float *a, float *b, float *c, int n)
 
 int main()
 {
-    int n = 1000000;
+    long long n = 100000000;
     float *A = malloc(n * sizeof *A);
     float *B = malloc(n * sizeof *B);
     float *C = malloc(n * sizeof *C);
@@ -21,8 +32,19 @@ int main()
         A[i] = i * 1.0f;
         B[i] = (n - i) * 1.0f;
     }
-
+    ////parllel run
+    double end_time, start_time;
+    start_time = omp_get_wtime();
     vec_add(A, B, C, n);
+    end_time = omp_get_wtime();
+    printf("timr fot parll run: %f \n", end_time - start_time);
+
+    //////////serial run
+    start_time = omp_get_wtime();
+    vec_add_serial(A, B, C, n);
+    end_time = omp_get_wtime();
+    printf("timr fot serial run: %f \n", end_time - start_time);
+
     printf("C[0]=%.1f, C[n-1]=%.1f\n", C[0], C[n - 1]);
 
     free(A);
